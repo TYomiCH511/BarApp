ae464f929c39fd87006dd166517e2f69e61991b8
TYomiCH <timankov511@gmail.com>
2020-10-18 23:47:18 +0300

    Customize table view cell

---
+++ BarApp/CustomizeTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomizeTableViewCell.swift
+//  BarApp
+//
+//  Created by Artem on 10/18/20.
+//
+
+import UIKit
+
+class CustomizeTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameOfPlace: UILabel!
+    @IBOutlet var locationOfPlace: UILabel!
+    @IBOutlet var typeOfPlace: UILabel!
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+}
