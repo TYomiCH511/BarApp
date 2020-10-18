6a69d2729542bbac08d9425523d90d8a7f388540
TYomiCH <timankov511@gmail.com>
2020-10-18 21:38:28 +0300

    Initial Commit

---
+++ BarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarApp
+//
+//  Created by Artem on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
